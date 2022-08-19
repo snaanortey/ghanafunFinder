960e4b27303f068510db25e6f31952a21424f851
Naa Norkor <nortey.sarah@gmail.com>
2022-08-19 01:44:07 +0100

    Added error handling - try and catch  features

--- routes/suggestActivity.js
+++ routes/suggestActivity.js
@@ -1,13 +1,7 @@
 const express = require("express");
 
-// Import file system
-const { writeFile } = require("node:fs/promises");
-
 // Import mongoose model
 const suggestionModel = require("../models/suggestion");
-
-// Import data from suggestions.json so it can be added to arrayOfSuggestedIdeas
-const arrayOfSuggestedIdeas = require("../suggestions.json");
 
 const { isActivityValid } = require("../util");
 
@@ -15,37 +9,29 @@ const app = express();
 
 // Allow users to suggest new activities via http POST requests on /suggest
 app.post("/suggest", async function (request, response) {
-  // The suggested activity is stored in the body of the http request
-  // const { body } = request;
-  // const suggestedActivity = body;
-
-  // The suggested activity is stored in the mongodb database
-  const suggestedActivity = new suggestionModel(request.body);
-
   // Calling the function to check that the object passed to the funtion is valid
-  const isValid = isActivityValid(suggestedActivity);
+  const isValid = isActivityValid(request.body);
 
-  //Validate the input from the user and return bad request if the input is invalid
+  //Validate the description property of the object and return bad request if the input is invalid
   if (!isValid) {
     response.status(400).send("Your suggestion is invalid. Please try again");
     return;
   }
 
-  // Suggested activity is saved and passed to databased
+  // create an instance of a MongoDB to be stroed to db
+  const suggestedActivity = new suggestionModel(request.body);
 
-  await suggestedActivity.save();
+  // Valid suggested activity is saved to the database
+  try {
+    await suggestedActivity.save();
+    response.send("Suggestion received, thank you!");
+    console.log("try done");
 
-  // Add new suggested activity to an array
-  // arrayOfSuggestedIdeas.push(suggestedActivity);
-
-  // console.log(arrayOfSuggestedIdeas);
-
-  // Save the array of suggested activities to the suggestions file (suggestions.json)
-  // const data = JSON.stringify(arrayOfSuggestedIdeas, null, 2);
-
-  // await writeFile("suggestions.json", data);
-
-  response.send("Suggestion received, thank you!");
+    // Any other error is caught and generates an invalid error response
+  } catch (error) {
+    response.status(400).send("Your suggestion is invalid. Please try again");
+    return;
+  }
 });
 
 module.exports = app;
