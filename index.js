b18485cd41a25b74d35f48437c21d5b11a5a6f48
Naa Norkor <nortey.sarah@gmail.com>
2022-08-25 00:07:51 +0100

    Installed and imported CORS

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 // Import express
 const express = require("express");
 
+// Import cors
+var cors = require("cors");
+
 const mongoose = require("mongoose");
 
 // Import random activity route
@@ -11,6 +14,9 @@ const randomActivity = require("./routes/randomActivity");
 
 // Import suggest activity route
 const suggestActivity = require("./routes/suggestActivity");
+
+// Import serve HTML route
+const serveHTML = require("./routes/serveHTML");
 
 // Connects the API server to MongoDB Atlas
 const { CONNECTIONS_STRING, PORT = 3000 } = process.env;
@@ -33,6 +39,8 @@ mongoose.connect(
 // Create an instance of an express app
 const server = express();
 
+server.use(cors());
+
 // for parsing application/json; see link >> https://expressjs.com/en/api.html#req.body
 // By default, the app server is not able to parse the json in the body of the http request
 // Teach the web server how to parse request body of content type: application/json
@@ -43,6 +51,10 @@ server.use(randomActivity);
 
 // Register the random activity route to be used here
 server.use(suggestActivity);
+
+// Register the serve HTML route to be used here
+server.use(serveHTML);
+server.use('/frontend', express.static('frontend'))
 
 // starting the App to listen for http request on port 3000
 server.listen(PORT, function () {
